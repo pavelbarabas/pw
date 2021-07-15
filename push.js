f628e1b3d3ee4aef2113ff9825508575f8252f31
Pavel <pavel.barabas@cometsystem.cz>
2021-07-15 09:34:09 +0200

    fix

--- push.js
+++ push.js
@@ -7,5 +7,15 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:pavel.barabas@cometsystem.cz', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {};
+  let sub = {
+      endpoint:
+      'https://fcm.googleapis.com/fcm/send/eSmcIefzcgo:APA91bGOGnPq4dDCXD5mb5nzKCEWzoZg4qvTJz0KTepZWMHjgJMIT2pJHd4bNizITQPJG68pKfuwmClniisxoZ6F6Q_QbXAUMbnJooJWKewhztOVIwNY5uukgzJ7PAVdV4n2ER0dc-VP',
+      expirationTime:null,
+      keys:{
+          p256dh:'BFRNzhB9BOboirY-ikpOJAqlr4ri3eaig9GsLld-PO5J8c3SohZDZr21wwR-kLDD-21CBsRH-Qhug2jqf_Y8zi8',
+          auth:'eZAVc6FAHUB7cFfJbe7qWg'
+        }
+    };
+
+
   push.sendNotification(sub, 'test message')
